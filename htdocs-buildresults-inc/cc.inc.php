f0366e5e2cf18f111cbd61fc34ef720a6584ba02
schristou88 <schristou88@gmail.com>
2013-05-22 16:35:05 -0500

    Initial Commit

---
+++ htdocs-buildresults-inc/cc.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+include("${projecthome}/htdocs-buildresults-inc/util.inc.php");
+include("${projecthome}/htdocs-buildresults-inc/cc_util.inc.php");
+include("${projecthome}/htdocs-buildresults-inc/navigation.inc.php");
+
+?>
